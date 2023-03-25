b893304e9d04d483ca9705e81ddfe42aae69c7f6
Haeseong Jeon <seong0428@gmail.com>
2023-03-25 17:14:57 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/invitation/InvitationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.invitation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvitationApplication
+
+fun main(args: Array<String>) {
+    runApplication<InvitationApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/invitation/InvitationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.invitation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvitationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
