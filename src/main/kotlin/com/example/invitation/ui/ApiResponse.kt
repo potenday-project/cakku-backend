b82fd32687dbf8ff8597d9d6915d5f9d2a9eae47
Haeseong Jeon <seong0428@gmail.com>
2023-03-27 01:13:50 +0900

    refactor: 중복되는 문자열 상수 처리

--- src/main/kotlin/com/example/invitation/ui/ApiResponse.kt
+++ src/main/kotlin/com/example/invitation/ui/ApiResponse.kt
@@ -6,33 +6,38 @@ class ApiResponse<T>(
     val data: T? = null,
 ) {
     companion object {
+        private const val successCode = "SUCCESS"
+        private const val successMessage = "SUCCESS"
+        private const val failureCode = "FAILURE"
+        private const val failureMessage = "FAILURE"
+
         fun success(): ApiResponse<Unit> {
             return ApiResponse(
-                code = "SUCCESS",
-                message = "SUCCESS",
+                code = successCode,
+                message = successMessage,
             )
         }
 
         fun <T> success(data: T): ApiResponse<T> {
             return ApiResponse(
-                code = "SUCCESS",
-                message = "SUCCESS",
+                code = successCode,
+                message = successMessage,
                 data = data,
             )
         }
 
         fun <T> success(contents: List<T>): ApiResponse<List<T>> {
             return ApiResponse(
-                code = "SUCCESS",
-                message = "SUCCESS",
+                code = successCode,
+                message = successMessage,
                 data = contents,
             )
         }
 
         fun failure(): ApiResponse<Unit> {
             return ApiResponse(
-                code = "FAILURE",
-                message = "FAILURE",
+                code = failureCode,
+                message = failureMessage,
             )
         }
     }
