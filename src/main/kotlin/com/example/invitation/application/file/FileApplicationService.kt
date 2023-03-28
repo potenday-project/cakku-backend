a17a64754226dda79ec6ab7fc00f4fdb0fd4cf04
Haeseong Jeon <seong0428@gmail.com>
2023-03-28 09:19:59 +0900

    feat: 파일 다운로드 헤더 추가

--- src/main/kotlin/com/example/invitation/application/file/FileApplicationService.kt
+++ src/main/kotlin/com/example/invitation/application/file/FileApplicationService.kt
@@ -5,6 +5,7 @@ import com.example.invitation.ui.file.FileResponse
 import org.springframework.stereotype.Component
 import java.io.InputStream
 import java.io.OutputStream
+import javax.servlet.http.HttpServletResponse
 
 @Component
 class FileApplicationService(
@@ -30,8 +31,11 @@ class FileApplicationService(
         }.toFileResponse()
     }
 
-    fun download(fileId: Long, outputStream: OutputStream) {
+    fun download(fileId: Long, outputStream: OutputStream, response: HttpServletResponse) {
         val file = fileService.findById(fileId) ?: throw FileNotFoundException("File not found. fileId: $fileId")
+        response.setContentLength(file.size.toInt())
+        response.contentType = file.contentType
+//        response.setHeader("Content-Disposition", "attachment; filename=${file.name}")
         storageService.download(
             outputStream,
             StorageDownloadRequestVo(
