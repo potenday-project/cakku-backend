3a0ceb29bfb69a925917a93706a35c2d6e5e0394
전해성 <seong0428@gmail.com>
2023-03-30 01:20:26 +0900

    config: cors 설정 추가 (#15)
    * feat: cors 설정 추가
    
    * feat: cors 설정 추가
    
    * fix: 컴파일 실패 해결

---
+++ src/main/kotlin/com/example/invitation/infrastructure/WebMvcConfig.kt
@@ -0,0 +1,21 @@
+package com.example.invitation.infrastructure
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebMvcConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/api/**")
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/com/example/invitation/ui/CorsTest.kt
@@ -0,0 +1,24 @@
+package com.example.invitation.ui
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CorsTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun testCors() {
+        mockMvc.perform(options("/api/v1/invitation-drafts"))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk)
+    }
+}
