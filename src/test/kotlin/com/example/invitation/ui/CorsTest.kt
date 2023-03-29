e38f3517b38f011fe45455820ef5dacaef2cfa4b
Haeseong Jeon <seong0428@gmail.com>
2023-03-30 01:03:03 +0900

    feat: cors 설정 추가

---
+++ src/test/kotlin/com/example/invitation/ui/CorsTest.kt
@@ -0,0 +1,22 @@
+package com.example.invitation.ui
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CorsTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun testCors() {
+        mockMvc.perform(options("/api/v1/invitations/{invitationId}", 1))
+            .andExpect(status().isOk)
+    }
+}
