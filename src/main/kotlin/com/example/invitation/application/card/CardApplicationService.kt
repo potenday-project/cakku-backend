41986839edc91dd72d9d394e70a6fbf2e9635f87
Haeseong Jeon <seong0428@gmail.com>
2023-04-01 02:46:32 +0900

    feat: card-template-item 목록 api 추가

--- src/main/kotlin/com/example/invitation/application/card/CardApplicationService.kt
+++ src/main/kotlin/com/example/invitation/application/card/CardApplicationService.kt
@@ -1,15 +1,27 @@
 package com.example.invitation.application.card
 
 import com.example.invitation.domain.card.template.CardTemplateService
+import com.example.invitation.domain.card.template.item.CardTemplateItemService
+import com.example.invitation.domain.invitation.InvitationDetailType
+import com.example.invitation.ui.card.CardTemplateItemResponse
 import com.example.invitation.ui.card.CardTemplateResponse
 import org.springframework.stereotype.Component
 
 @Component
 class CardApplicationService(
     private val cardTemplateService: CardTemplateService,
+    private val cardTemplateItemService: CardTemplateItemService,
 ) {
     fun getCardTemplates(): List<CardTemplateResponse> {
         return cardTemplateService.getCardTemplates()
             .map { it.toDto() }
+    }
+
+    fun getCardTemplateItems(
+        invitationDetailType: InvitationDetailType?,
+    ): List<CardTemplateItemResponse> {
+        return cardTemplateItemService.getCardTemplateItems(
+            invitationDetailType = invitationDetailType
+        ).map { it.toDto() }
     }
 }
---
+++ src/main/kotlin/com/example/invitation/ui/card/CardTemplateItemController.kt
@@ -0,0 +1,24 @@
+package com.example.invitation.ui.card
+
+import com.example.invitation.application.card.CardApplicationService
+import com.example.invitation.domain.invitation.InvitationDetailType
+import com.example.invitation.ui.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/v1/card-template-items")
+@RestController
+class CardTemplateItemController(
+    private val cardApplicationService: CardApplicationService
+) {
+    @GetMapping
+    fun getCardTemplateItems(
+        @RequestParam(required = false) invitationDetailType: InvitationDetailType?
+    ): ApiResponse<List<CardTemplateItemResponse>> {
+        return ApiResponse.success(
+            data = cardApplicationService.getCardTemplateItems(invitationDetailType)
+        )
+    }
+}
