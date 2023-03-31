b0564abc46b5995b862cf15901248518cdd9a52f
Haeseong Jeon <seong0428@gmail.com>
2023-03-31 20:46:39 +0900

    fix: 빌드 실패 해결

--- src/test/kotlin/com/example/invitation/ui/invitation/InvitationControllerTest.kt
+++ src/test/kotlin/com/example/invitation/ui/invitation/InvitationControllerTest.kt
@@ -4,6 +4,8 @@ import com.example.invitation.domain.card.template.CardTemplate
 import com.example.invitation.domain.card.template.CardTemplateRepository
 import com.example.invitation.domain.card.template.item.CardTemplateItem
 import com.example.invitation.domain.card.template.item.CardTemplateItemRepository
+import com.example.invitation.domain.invitation.InvitationDetailType
+import com.example.invitation.domain.invitation.InvitationType
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -38,20 +40,39 @@ class InvitationControllerTest {
         val cardTemplateId = 1L
         val cardTemplate = CardTemplate(
             cardTemplateId = cardTemplateId,
+            invitationType = InvitationType.CASUAL,
             name = "name",
-            backgroundImageUrl = "imageUrl",
+            imageUrl = "imageUrl",
         )
         cardTemplateRepository.save(cardTemplate)
         cardTemplateItemRepository.saveAll(
             listOf(
                 CardTemplateItem(
                     cardTemplateItemId = 1L,
+                    invitationDetailType = InvitationDetailType.MEAL,
+                    imageUrl = "imageUrl",
+                    name = "name",
+                    emoji = "emoji",
+                    x = 0,
+                    y = 0,
                 ),
                 CardTemplateItem(
                     cardTemplateItemId = 2L,
+                    invitationDetailType = InvitationDetailType.MEAL,
+                    imageUrl = "imageUrl",
+                    name = "name",
+                    emoji = "emoji",
+                    x = 1,
+                    y = 0,
                 ),
                 CardTemplateItem(
                     cardTemplateItemId = 3L,
+                    invitationDetailType = InvitationDetailType.MEAL,
+                    imageUrl = "imageUrl",
+                    name = "name",
+                    emoji = "emoji",
+                    x = 2,
+                    y = 0,
                 ),
             )
         )
