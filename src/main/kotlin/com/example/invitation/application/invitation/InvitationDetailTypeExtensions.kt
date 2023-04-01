4336cf9eb6b2e16a07404709b3f1e053c08055b8
Haeseong Jeon <seong0428@gmail.com>
2023-04-01 15:56:00 +0900

    feat: invitationTypeIndex 추가

--- src/main/kotlin/com/example/invitation/application/invitation/InvitationDetailTypeExtensions.kt
+++ src/main/kotlin/com/example/invitation/application/invitation/InvitationDetailTypeExtensions.kt
@@ -7,6 +7,7 @@ fun InvitationDetailType.toDto(): InvitationDetailTypeResponse {
     return InvitationDetailTypeResponse(
         invitationDetailTypeId = invitationDetailTypeId.toString(),
         invitationType = invitationType.name,
+        invitationTypeIndex = invitationType.displayIndex,
         name = name,
         emoji = emoji,
         description = description,
