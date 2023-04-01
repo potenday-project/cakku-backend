a41616b558dc5f3ce7eb54e7e8306954d514e8db
전해성 <seong0428@gmail.com>
2023-04-01 16:11:09 +0900

    feat: 응답 모델 개선 (#29)
    * test: localTime 테스트 추가
    
    * feat: invitationTypeIndex 추가

--- src/main/kotlin/com/example/invitation/ui/invitation/InvitationDetailTypeResponse.kt
+++ src/main/kotlin/com/example/invitation/ui/invitation/InvitationDetailTypeResponse.kt
@@ -3,6 +3,7 @@ package com.example.invitation.ui.invitation
 data class InvitationDetailTypeResponse(
     val invitationDetailTypeId: String,
     val invitationType: String,
+    val invitationTypeIndex: Int,
     val name: String,
     val emoji: String,
     val description: String,
--- src/test/kotlin/com/example/invitation/ui/invitation/InvitationControllerTest.kt
+++ src/test/kotlin/com/example/invitation/ui/invitation/InvitationControllerTest.kt
@@ -121,7 +121,8 @@ class InvitationControllerTest {
                         "invitationDetailTypeId": ${invitationDetailType.invitationDetailTypeId},
                         "summary": "밥 한 번 먹자",
                         "description": "K-밥약",
-                        "cardTemplateItemIds": [1, 2, 3]
+                        "cardTemplateItemIds": [1, 2, 3],
+                        "time": "12:34"
                     }""".trimIndent()
                 )
         )
