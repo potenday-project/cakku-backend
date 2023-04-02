69f34e99f2e3a0ff45a71c80afddb35a9c4927e6
전해성 <seong0428@gmail.com>
2023-04-02 19:08:14 +0900

    feat: 이벤트 추가 (#33)

--- src/main/kotlin/com/example/invitation/ui/rating/RatingController.kt
+++ src/main/kotlin/com/example/invitation/ui/rating/RatingController.kt
@@ -1,6 +1,8 @@
 package com.example.invitation.ui.rating
 
 import com.example.invitation.application.rating.RatingApplicationService
+import com.example.invitation.domain.analytics.AnalyticsEvent
+import com.example.invitation.domain.analytics.AnalyticsEventPublished
 import com.example.invitation.ui.ApiResponse
 import org.springframework.web.bind.annotation.*
 
@@ -10,6 +12,7 @@ class RatingController(
     private val ratingApplicationService: RatingApplicationService,
 ) {
     @PostMapping
+    @AnalyticsEventPublished(event = AnalyticsEvent.INVITATION_RATED)
     fun createRating(
         @RequestBody ratingRequest: RatingRequest,
     ): ApiResponse<RatingResponse> {
